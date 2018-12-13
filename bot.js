c31523c5d0ef1a8e0c0d1e8fadcd2451307e728f
spam12356 <45719903+spam12356@users.noreply.github.com>
2018-12-13 16:11:40 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,7 +6,7 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("521277271663706113")
 setInterval(function() {
-channel.send(`spamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspamspam`);
+channel.send(`okokokokokokokokokokokkokkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk`);
 }, 30)
 })
 
